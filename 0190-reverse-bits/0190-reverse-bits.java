2b1522c11d2dee1248448757dbb774fd3b05d77e
Orlando Kodjo <57968753+orla-ske@users.noreply.github.com>
2023-10-06 12:11:36 +0000

    Time: 0 ms (100%), Space: 40.6 MB (63.03%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,14 +1,16 @@
 public class Solution {
-    
-    public int reverseBits(int num) {
-        
-        num = ((num & 0xffff0000) >>> 16) | ((num & 0x0000ffff) << 16);
-        num = ((num & 0xff00ff00) >>> 8) | ((num & 0x00ff00ff) << 8);
-        num = ((num & 0xf0f0f0f0) >>> 4) | ((num & 0x0f0f0f0f) << 4);
-        num = ((num & 0xcccccccc) >>> 2) | ((num & 0x33333333) << 2);
-        num = ((num & 0xaaaaaaaa) >>> 1) | ((num & 0x55555555) << 1);
-        
-        return num;
-        
+
+    public int reverseBits(int n) {
+        int ans = 0;
+        for(int i=0;i<Integer.SIZE;i++) {
+
+            int k = (int)(n & 1);
+
+            if(k > 0) {
+                ans += (int)(1L << (31-i));
+            }
+            n = n >> 1;
+        }
+        return ans;
     }
 }
