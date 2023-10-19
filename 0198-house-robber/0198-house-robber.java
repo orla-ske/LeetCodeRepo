23497f4a95ee9bd7819647392e47ad762ffad638
Orlando Kodjo <57968753+orla-ske@users.noreply.github.com>
2023-10-19 13:14:36 +0000

    Time: 0 ms (100%), Space: 39.8 MB (56.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
-        int prev = nums[0],prev2=0,curr;
-        for(int i=1;i<nums.length;i++)
-        {
-            int notSkippingCurrIDX = prev2 + nums[i];
-            int skippingCurrIDX = prev;
-            curr = Math.max(notSkippingCurrIDX,skippingCurrIDX);
+        int prev = nums[0]; 
+        int prev2 = 0; 
+        int curr = 0; 
+
+
+        for(int i = 1; i<nums.length;i++){
+            int currInd = prev; 
+            int skipInd = prev2+nums[i]; 
+
+            curr = Math.max(currInd, skipInd); 
+
             prev2 = prev;
             prev = curr;
         }
-        return prev;
 
+        return prev;
     }
 }
