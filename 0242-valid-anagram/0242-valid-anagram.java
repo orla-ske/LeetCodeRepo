688b992b9281bef211631c5575992fb8fd7a9fc9
Orlando Kodjo <57968753+orla-ske@users.noreply.github.com>
2024-07-24 16:29:28 +0000

    Time: 4 ms (70.42%), Space: 44.5 MB (40.67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char [] sArray = s.toCharArray(); 
+        char [] tArray = t.toCharArray(); 
+
+        if(s.length()!= t.length() ){
+            return false;
+        }
+
+        Arrays.sort(sArray); 
+        Arrays.sort(tArray); 
+
+        for(int i = 0; i<sArray.length;i++){
+            if(sArray[i]!=tArray[i]){
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+}
